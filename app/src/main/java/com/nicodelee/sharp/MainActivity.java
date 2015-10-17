c1634567e79e36b092cc6fbc0c9e507112f1e123
NicodeLee <lirizhilirizhi@163.com>
2015-10-17 15:32:55 +0800

    gitignore

--- app/src/main/java/com/nicodelee/sharp/MainActivity.java
+++ app/src/main/java/com/nicodelee/sharp/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
             .setAction("Action", null)
             .show();
+
       }
     });
   }
